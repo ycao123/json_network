51a48c3176c2294a42a8b97965c0297b9446c682
ycao123 <ycao123@asu.edu>
2022-04-29 15:27:59 -0700

    Used python implimentation of prime-number generation in order to use 1024-bit integers

--- test.c
+++ test.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "rsa.c"
+//#include "rsa.c"
+#include <assert.h>
 
 int main(void)
 {
-    for (int i = 234; i < 244; i++)
-    {
-        BIGINT left = randrange(234224342,238492348234);
-        BIGINT right = randrange(132321,234423423);
-        BIGINT mod= randrange(132132,342324324);
-        BIGINT num1 = modulo(left, right, mod);
-        printf("Iteration %d\n", i);
-    }
+    assert((float)(1/2) == 0.5);
 }
